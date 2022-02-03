7e869a1f95116e3fd2efa00a0fe7741bdfec3b51
Julia Thompson <jewelst2014@gmail.com>
2022-02-03 12:35:28 -0500

    Started dosageManager

---
+++ dosageManager.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+from dosageEntry import PatientInfoEntry
+from dosageTableView import Application
+from dosageCalculations import Patient, Emergency, anesthesiaAnalgesia, AdvancedLifeSupport
+
+class DosageManager():
+    def __init__(self):
+        self.root = Tk()
+        self.screen = None
+
+    def disaplayPatientIntroEntry(self):
+        self.root.title("Patient Information")
+        self.screen = PatientInfoEntry(master = self.root, callback_on_selected = self.onclose_PatientIntroEntry)
