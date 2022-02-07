f83985071c732f6c5538edf7076489c96a0a3d76
Julia Thompson <jewelst2014@gmail.com>
2022-02-07 11:46:31 -0500

    Connected both views via a manager file

--- dosageManager.py
+++ dosageManager.py
@@ -9,6 +9,27 @@ class DosageManager():
         self.root = Tk()
         self.screen = None
 
-    def disaplayPatientIntroEntry(self):
+    def displayPatientIntroEntry(self):
         self.root.title("Patient Information")
         self.screen = PatientInfoEntry(master = self.root, callback_on_selected = self.onclose_PatientIntroEntry)
+
+    def onclose_PatientIntroEntry(self, patient, owner, species, weight):
+        self.patient = patient
+        self.owner = owner
+        self.species = species
+        self.weight = weight
+
+        self.screen.destroy()
+
+        self.displayDosageTable()
+
+    def displayDosageTable(self):
+        self.root.title("Dosages")
+        self.screen = Application(master = self.root)
+
+def main():
+    calc = DosageManager()
+    calc.displayPatientIntroEntry()
+    calc.root.mainloop()
+
+main()
