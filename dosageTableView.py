4d347aee695a8fb1748956ecc0188199e3fde63c
Julia Thompson <jewelst2014@gmail.com>
2022-02-09 12:31:27 -0500

    ALS Table progress!!

--- dosageTableView.py
+++ dosageTableView.py
@@ -19,6 +19,7 @@ class Application(Frame):
 
         self.create_widgets_generalInfo()
         self.create_widgets_emergency()
+        self.create_widgets_ALS()
         self.create_widgets_anesthesia
         self.kgs = weight
         self.ownerName = owner
@@ -109,8 +110,58 @@ class Application(Frame):
             
 
     def create_widgets_ALS(self):
-        # TODO
-        pass
+        Label(self, text = "Advanced Life Support (ALS)", bg = "#56b2e8", font = "Impact 12", fg = "white", relief = "solid", bd = 1).grid(row = 17, column = 0, columnspan = 6, sticky = W+E, padx = (20,0), pady = (10,0))
+
+        Label(self, text = "Drug", bg = "white", font = "Arial 9 bold", relief = "solid", bd = 1).grid(row = 18, rowspan = 2, column = 0, sticky = W+E+N+S, padx = (20,0))
+
+        Label(self, text = "Conc. mg/mL", bg = "white", font = "Arial 9 bold", relief = "solid", bd = 1).grid(row = 18, rowspan = 2, column = 1, sticky = W+E+N+S)
+
+        Label(self, text = "Range mg/kg", bg = "white", font = "Arial 9 bold", relief = "solid", bd = 1).grid(row = 18, rowspan = 2, column = 2, sticky = W+E+N+S)
+
+        Label(self, text = "Species", bg = "white", font = "Arial 9 bold", relief = "solid", bd = 1).grid(row = 18, rowspan = 2, column = 3, sticky = W+E+N+S)
+    
+        Label(self, text = "Volume (mLs)", bg = "white", font = "Arial 9 bold", relief = "solid", bd = 1).grid(row = 18, column = 4, columnspan = 2, sticky = W+E+N+S)
+
+        Label(self, text = "Minimum", bg = "white", relief = "solid", bd = 1).grid(row = 19, column = 4, sticky = W+E+N+S)
+
+        Label(self, text = "Maximum", bg = "white", relief = "solid", bd = 1).grid(row = 19, column = 5, sticky = W+E+N+S)
+
+
+        # fix this code v
+        f = open("namesALS.txt")
+        row_num = 20
+        column_num = 0
+        x = 20
+
+
+        for line in f:
+            l = line.split("; ")
+            x = 20
+            for item in l:
+                Label(self, text = item, bg = "white", relief = "solid", bd = 1).grid(row = row_num, column = column_num, sticky = W+E+N+S, padx = (x, 0))
+                if x == 20:
+                    x = 0
+                column_num += 1
+            row_num += 2
+            column_num = 0
+
+        row_num = 20
+        column_num = 4
+
+        i = 0
+        for num in Emergency(float(self.kgs)).returnMedicineList():
+            num = f"{num:.2f}"
+            l = Label(self, text = num, bg = "white", relief = "solid", bd = 1)
+            if row_num % 2 == 0:
+                l.grid(row = row_num, column = column_num, sticky = W+E+N+S)
+            else:
+                l.grid(row = row_num - 1, column = column_num, sticky = W+E+N+S)
+            row_num += 1
+            i += 1
+            if column_num == 5:
+                column_num = 4
+            else:
+                column_num += 1
     
 
     def create_widgets_anesthesia(self):
