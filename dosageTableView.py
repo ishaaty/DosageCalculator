beb5e320b09348a31e4518070095b33787336e18
ishaaty <ishatyagi888@gmail.com>
2022-02-11 12:01:57 -0500

    added anesthesia loop

--- dosageTableView.py
+++ dosageTableView.py
@@ -164,9 +164,9 @@ class Application(Frame):
 
     def create_widgets_anesthesia(self):
 
-        Label(self, text = "Anesthesia and Analegesia Medications", bg = "#56b2e8", font = "Impact 12", fg = "white", relief = "solid", bd = 1).grid(row = 3, column = 6, columnspan = 6, sticky = W+E, padx = (10,0))
+        Label(self, text = "Anesthesia and Analegesia Medications", bg = "#56b2e8", font = "Impact 12", fg = "white", relief = "solid", bd = 1).grid(row = 3, column = 6, columnspan = 6, sticky = W+E, padx = (20,0))
 
-        Label(self, text = "Drug", bg = "white", font = "Arial 9 bold", relief = "solid", bd = 1).grid(row = 4, rowspan = 2, column = 6, sticky = W+E+N+S, padx = (10,0))
+        Label(self, text = "Drug", bg = "white", font = "Arial 9 bold", relief = "solid", bd = 1).grid(row = 4, rowspan = 2, column = 6, sticky = W+E+N+S, padx = (20,0))
 
         Label(self, text = "Conc. mg/mL", bg = "white", font = "Arial 9 bold", relief = "solid", bd = 1).grid(row = 4, rowspan = 2, column = 7, sticky = W+E+N+S)
 
@@ -179,6 +179,43 @@ class Application(Frame):
         Label(self, text = "Minimum", bg = "white", relief = "solid", bd = 1).grid(row = 5, column = 10, sticky = W+E+N+S)
 
         Label(self, text = "Maximum", bg = "white", relief = "solid", bd = 1).grid(row = 5, column = 11, sticky = W+E+N+S)
+
+        f = open("anesthesiaAnalgesiaFile.txt")
+        row_num = 6
+        column_num = 6
+        x = 20
+
+        for line in f:
+            l = line.split("; ")
+            x = 20
+            for item in l:
+                Label(self, text = item, bg = "white", relief = "solid", bd = 1).grid(row = row_num, column = column_num, sticky = W+E+N+S, padx = (x, 0))
+                if x == 20:
+                    x = 0
+                column_num += 1
+            row_num += 2
+            column_num = 6
+
+        row_num = 6
+        column_num = 10
+
+
+        for num in anesthesiaAnalgesia(float(self.kgs)).returnMedicine():
+            if num == 0:
+                l = Label(self, text = "", bg = "#56b2e8", relief = "solid", bd = 1)
+            else:
+                num = f"{num:.2f}"
+                l = Label(self, text = num, bg = "white", relief = "solid", bd = 1)
+            if row_num % 2 == 0:
+                l.grid(row = row_num, column = column_num, sticky = W+E+N+S)
+            else:
+                l.grid(row = row_num - 1, column = column_num, sticky = W+E+N+S)
+            row_num += 1
+            if column_num == 11:
+                column_num = 10
+            else:
+                column_num += 1
+
 
     def create_widgets_buttons(self):
         Button(self, text = "Close Form", bg = "#56b2e8", fg = "white", font = "Impact 8", command = self.form_closed).grid(row = 0, column = 7)
