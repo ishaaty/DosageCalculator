822ebac8506d0cb715b7e0feb7eb4e92e8b1a313
ishaaty <ishatyagi888@gmail.com>
2022-03-02 07:49:33 -0500

    added weight type section

--- dosageEntry.py
+++ dosageEntry.py
@@ -24,20 +24,36 @@ class PatientInfoEntry(Frame):
         row_value = 1
         self.species = StringVar()
         self.species.set(None)
+
         for animal in speciesList:
             Radiobutton(self, text = animal, variable = self.species, value = animal).grid(row = row_value, column = 3)
             row_value += 1
         
+        Label(self, text = "Select Weight Unit", fg = "#56b2e8", font = "Arial 10 underline").grid(row = 0, column = 4, sticky = S)
+        weightList = ["kgs", "lbs"]
+        row_value = 1
+        self.weight = StringVar()
+        self.weight.set(None)
+
+        for i in weightList:
+            Radiobutton(self, text = i, variable = self.weight, value = i).grid(row = row_value, column = 4)
+            row_value += 1
+
         Label(self, text = "Injectable Perianesthetic Medications for canine/feline")
 
         Label(self, text = "Weight:").grid(row = 4, column = 0)
         self.weight_ent = Entry(self)
+
         self.weight_ent.grid(row = 4, column = 1)
-        Label(self, text = "KGS").grid(row = 4, column = 2, sticky = W)
 
         Label(self, text = "").grid(row = 3, column = 3)
 
         Button(self, text = "Submit", bg = "#56b2e8", command = self.on_button_press).grid(row = 4, column = 3)
 
+
     def on_button_press(self):
+        
         self.callback_on_selected(self.patient_ent.get(), self.owner_ent.get(), self.species.get(), self.weight_ent.get())
+
+
+
